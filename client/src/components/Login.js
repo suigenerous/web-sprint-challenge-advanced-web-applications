9ad1afb1ac1b1e76e7e047f9571370fd60b354de
William Herman <williamajherman@gmail.com>
2020-09-18 11:16:33 -0400

    login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+ 
+  const [formValues, setFormValues] = useState({
+    username: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState('');
+
+  const submitCredentials = (credentials) => {
+    return axiosWithAuth()
+        .post('/api/login', { username: credentials.username, password: credentials.password })
+        .then (res => {
+            localStorage.setItem('token', res.data.payload);
+            console.log(res.data.payload);
+        })
+        .catch(err => {
+            console.log(err);
+            setErrors(err.response.data.error);
+        })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log(formValues)
+    submitCredentials(formValues)
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username
+          <input
+              type = "text"
+              name="username"
+              value = {formValues.username}
+              onChange = {e => setFormValues({...formValues, username: e.target.value})}
+          />
+          </label>
+          <label htmlFor="password">Password
+              <input
+              type = "password"
+              name="password"
+              value = {formValues.password}
+              onChange = {e => setFormValues({...formValues, password: e.target.value})}
+              />
+          </label>
+          <button type = "submit">Login</button>
+        </form>
+        <p>{errors}</p>
+    </div>
   );
 };
 
